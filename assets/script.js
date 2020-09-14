a751a3f85c8cf875f841e327ad6c3e056fbdd410
Timothy Forbes <timothy.forbes259@gmail.com>
2020-09-14 12:55:36 +0930

    started time and date table

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,11 @@
+// To display todays date
 function todaysDate() {
     var date = `${moment().format('dddd')}, ${moment().format('ll')}`;
     $("#currentDay").append(date);
 }
 todaysDate();
 
+// change table color based on the time of day
+function tableColorChange () {
 
+}
